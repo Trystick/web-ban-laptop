a5dc901b4fddb29cdc542f2c5962ef889499cc3e
Tuan Anh Ha <tuananhha@Tuans-MBP.fritz.box>
2022-04-25 23:36:46 +0200

    frontend part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource } from 'react-admin'
+import simpleRestProvider from 'ra-data-simple-rest'
+
+import { listProducts, editProduct, createProduct } from './components/Products'
+import { listUsers, editUser, createUser } from './components/Users'
+import AdminPanel from './components/AdminPanel'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Admin
+        dashboard={AdminPanel}
+        dataProvider={simpleRestProvider('http://localhost:3000')}
+      >
+        <Resource
+          name='products'
+          list={listProducts}
+          edit={editProduct}
+          create={createProduct}
+        />
+        <Resource
+          name='users'
+          list={listUsers}
+          edit={editUser}
+          create={createUser}
+        />
+      </Admin>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
