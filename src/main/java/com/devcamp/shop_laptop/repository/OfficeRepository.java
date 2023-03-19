31aef7a4814c0d95c597d6b74a200231e7e47afc
DESKTOP-2SI9LLN\jclov <trmkhang2001@gmail.com>
2023-03-19 17:12:57 +0700

    push code lan 1 poroduct + users

--- src/main/java/com/devcamp/shop_laptop/repository/OfficeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devcamp.shop_laptop.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devcamp.shop_laptop.model.Office;
-public interface OfficeRepository extends JpaRepository<Office, Integer>{
-    
-}
--- src/main/java/com/devcamp/shop_laptop/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devcamp.shop_laptop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devcamp.shop_laptop.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
